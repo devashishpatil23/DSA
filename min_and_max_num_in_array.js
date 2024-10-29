f7a164462bf1cb44e87d7ba32a7436ebd095fbc5
devashishpatil <devashishpatil23@gmail.com>
2024-10-29 15:30:38 +0530

    find ktn min and max value

--- min_and_max_num_in_array.js
+++ min_and_max_num_in_array.js
@@ -3,7 +3,7 @@ function findMinMax(arr) {
   let min = arr[0];
   let max = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i < arr.length -1; i++) {
     if (arr[i] < min) {
       min = arr[i];
     }
